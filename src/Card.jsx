82aa0e6bd3388e53bf385f07f50ac3971d7a00c8
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2023-09-27 21:06:25 +0200

    ändrat text

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,9 +9,9 @@ function Card(props) {
         <div className="flex flex-col p-10 justify-center border border-gray-200 rounded-md shadow-xl">
           <img className="abolute inset-x-0 w-80 " src={imgSrc} alt={imgAlt} />
           <h5 className="flex font-mono text-xl mt-2 tex text-gray-600 ">
-            Detta är ett Card
+            Card
           </h5>
-          <p className="text-gray-500">Ett card med en bild!</p>
+          <p className="text-gray-500">Click here!</p>
         </div>
       </a>
     </div>
